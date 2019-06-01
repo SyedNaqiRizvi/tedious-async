516f73ae6ccb8271cd73967a041fedf9fb3b8796
Naqi Rizvi <rizvisyednaqi@gmail.com>
2019-05-31 20:59:52 -0400

    chore: added travis ci

--- api/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mapSQLRowsToJSONList = (rows) => rows.map(mapSQLRowToJSON);
-exports.mapSQLRowsToJSONList = mapSQLRowsToJSONList;
-const mapSQLRowToJSON = (row) => row.reduce(addColumnAsJSONField, {});
-exports.mapSQLRowToJSON = mapSQLRowToJSON;
-const addColumnAsJSONField = (jsonRow, column) => (Object.assign({}, jsonRow, { [column.metadata.colName]: column.value }));
-exports.addColumnAsJSONField = addColumnAsJSONField;
-//# sourceMappingURL=utils.js.map
